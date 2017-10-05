5a47b6f1c8522db77100ee05f15a05c2249c97e4
ppreyer <parker.preyer1@gmail.com>
2017-10-05 09:20:49 -0400

    gif animation, search bar responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-
 var moodArray = ['happy', 'sad', 'mad', 'elated', 'frustrated', 'bewildered', 'confused', 'brave', 'modest', 'arrogant', 'talkative', 'bloated'];
 var renderMoodButtonsSection = $('#buttonSection');
 var renderGifImages = $('#gifImages');
 
 function displayMoodButtons() {
+  console.log("ENTER displayMoodButtons");
+
   renderMoodButtonsSection.empty();
   for(var i = 0; i < moodArray.length; i++) {
     var mood = moodArray[i];
@@ -19,16 +19,36 @@ function displayMoodButtons() {
                     'background-color': '#20B2AA',
                     'color': '#FFFFFF'
                   })
-    renderMoodButtonsSection.append(moodButton);
+    // renderMoodButtonsSection.append(moodButton);
+    
     $('.mood-button').hover(function(){
-        $(this).css('background-color', '#3e8e41');
+        $(this).css('box-shadow', '0 5px #666');
         }, function(){
-        $(this).css('background-color', '#20B2AA');
-    });
+        $(this).css('box-shadow', 'none');
+    })
+    
+    $('.mood-button').focus(function(){
+      $(this).css({
+        'outline': 'none'
+      })
+    })
+
+    $('#buttonSection').append(moodButton);
+//     }, function() {
+//       $(this).css({
+//         'box-shadow': 'none',
+//         'transform': 'none'
+//       })
+//     });
+//   }
+// }
+    }
   }
 
-  $('.mood-button').on('click', function(){
-  $(renderGifImages).empty();
+$(document).on('click','.mood-button', function(){
+  console.log("CLICKED MOOD");
+
+  $('#gifImages').empty();
   var moodSearchTerm = $(this).attr('data-name'); 
   var queryUrl = 'http://api.giphy.com/v1/gifs/search?q=' + moodSearchTerm + '&api_key=c8UVbAGIEcaESZJ6u8wKS7DLHbBnKc8F&limit=10';
 
@@ -37,16 +57,25 @@ $.ajax({
   method: 'GET'
 }).done(function(response) {
   var results = response.data;
+  console.log(results.length);
+
   for(var i = 0; i < results.length; i++) {
+    console.log(i);
+
     var moodGifDiv = $('<div>');
     moodGifDiv.addClass('mood-image-div');
     var rating = results[i].rating
     var moodGifImageRating = $('<p>').text(`Rating: ${rating}`);
-    var moodGifImage = $('<img>'); 
-    moodGifImage.attr('src', results[i].images.fixed_height.url);
+    var moodGifImage = $("<img id='moodImage'>"); 
+    moodGifImage.attr('src', results[i].images.fixed_height_still.url);
+    moodGifImage.attr({'data-animate' : results[i].images.fixed_height.url});
+    moodGifImage.attr({'data-state' : 'still'});
+    moodGifImage.attr({'data-still' : results[i].images.fixed_height_still.url});
     moodGifDiv.append(moodGifImageRating);
     moodGifDiv.append(moodGifImage);
-    renderGifImages.append(moodGifDiv);
+    console.log(moodGifDiv);
+    //renderGifImages.append(moodGifDiv);
+    $('#gifImages').append(moodGifDiv);
     $('.mood-image-div').css({
       'float': 'left',
       'padding': 10
@@ -59,17 +88,39 @@ $.ajax({
   }
 })
 
-})
+});
 
-}
+$(document).on('click', '#moodImage', function(){
+  console.log("IMAGE CLICK");
+  var state = $(this).attr('data-state');
+  if(state === 'still') {
+    $(this).attr('src', $(this).attr('data-animate'));
+    $(this).attr('data-state', 'animate');
+    console.log('THIS IS THE CURRENT STATE', state);
+  } else {
+    $(this).attr('src', $(this).attr('data-still'));
+    $(this).attr('data-state', 'still');
+    console.log('THIS IS THE CURRENT STATE', state)
+  }
+});
 
 $('#add-mood').on('click', function() {
   event.preventDefault();
   var moodSearchValue = $('#mood-input').val().trim();
-  moodArray.push(moodSearchValue);
-  displayMoodButtons();
+  if(moodSearchValue === '' || moodArray.indexOf(moodSearchValue.toLowerCase()) > -1) {
+    return;
+  } else {
+      moodArray.push(moodSearchValue);
+      displayMoodButtons();
+
+      $('input:text').focus(function(){
+      $(this).val('');
+      });
+    }
 });
 
-displayMoodButtons();
+$(document).ready(function() {
+  console.log("DOCUMENT READY");
 
+  displayMoodButtons();
 });
